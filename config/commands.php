846653b3c04e68f91782c16dcc57bce4a3a70c23
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2019-07-29 12:36:04 +0430

    edit config

--- config/commands.php
+++ config/commands.php
@@ -60,6 +60,10 @@ return [
         Illuminate\Console\Scheduling\ScheduleRunCommand::class,
         Illuminate\Console\Scheduling\ScheduleFinishCommand::class,
         Illuminate\Foundation\Console\VendorPublishCommand::class,
+        LaravelZero\Framework\Commands\InstallCommand::class,
+        LaravelZero\Framework\Commands\MakeCommand::class,
+        LaravelZero\Framework\Commands\RenameCommand::class,
+        LaravelZero\Framework\Commands\BuildCommand::class,
     ],
 
     /*
