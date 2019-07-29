31ba279d4f7dee26c072902393d1312a3ecf2d59
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2019-07-29 12:00:25 +0430

    gp start

--- app/Commands/Start.php
+++ app/Commands/Start.php
@@ -7,36 +7,58 @@ use LaravelZero\Framework\Commands\Command;
 
 class Start extends Command
 {
-    /**
-     * The signature of the command.
-     *
-     * @var string
-     */
-    protected $signature = 'command:name';
+    protected $signature = 'start';
 
-    /**
-     * The description of the command.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+    protected $description = 'start game';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
+    private $names = [];
+
     public function handle()
     {
-        //
+        $names = [];
+        while (true) {
+            $input = $this->ask('Enter Player name:');
+            if ($input == 'exit' || $input == '0') {
+                break;
+            }
+            $names[] = $input;
+        }
+        shuffle($names);
+
+        $this->names = $names;
+
+        while (count($this->names) > 1) {
+            $this->alert('^Next Round^');
+            $competitors = $this->planner($this->names);
+            $this->names = [];
+
+            foreach ($competitors as $competitor) {
+                if (count($competitor) == 1) {
+                    $this->names[] = $competitor[0];
+                    break;
+                }
+
+                do{
+                    $choice = $this->ask($competitor[0] . ' vs. ' . $competitor[1]);
+                }while(! in_array($choice, $competitor));
+
+                $this->names[] = $choice;
+//                $this->names[] = $this->choice('winner: ', $competitor);
+            }
+        }
+
+        $this->info('Winner is: ' . $this->names[0]);
     }
 
-    /**
-     * Define the command's schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
-     * @return void
-     */
+    private function planner($names)
+    {
+        if (count($names) % 2 == 1) {
+            $names = array_reverse($names);
+        }
+
+        return array_chunk($names, 2);
+    }
+
     public function schedule(Schedule $schedule): void
     {
         // $schedule->command(static::class)->everyMinute();
